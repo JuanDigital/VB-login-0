774917f72ea6e5dc8af859f85cbec5239da922cd
JuanDigital <94965256+JuanDigital@users.noreply.github.com>
2022-01-07 17:11:33 -0600

    como hacer un login con VB

---
+++ login/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" And TextBox2.Text = "" Then
+            MsgBox("Los campos se encuentran vacios")
+        Else
+            If TextBox1.Text = "" Then
+                MsgBox("El campo USUARIO se encuentra vacio")
+            Else
+                If TextBox2.Text = "" Then
+                    MsgBox("El campo CONTRASEÑA se encuentra vacio")
+                Else
+                    If TextBox1.Text = "TuUsuario" And TextBox2.Text = "TuContraseña" Then
+                        Me.Hide()
+                        form2.show()
+                    Else
+                        MsgBox("Los datos son incorrectos")
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+End Class
